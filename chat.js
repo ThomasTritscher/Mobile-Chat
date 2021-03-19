7563c84ba1a9e30818defebe303cdebf1e4a4259
ThomasTritscher <thomas.tritscher33@googlemail.com>
2021-03-19 09:50:06 +0100

    update

--- chat.js
+++ chat.js
@@ -1,46 +1,47 @@
 let messages = [];
 
-let users = [{
-  'user_img': 'img/female.jpg',
-  'lastname': 'Kruse',
-  'firstname': 'Maren',
-  'birth': '20.04.1983',
-  'mail': 'maren.kruse@gmail.com',
-}, {
-  'user_img': 'img/female2.jpg',
-  'lastname': 'Meier',
-  'firstname': 'Kathrin',
-  'birth': '09.10.1990',
-  'mail': 'kathrin.meier@gmail.com',
-},
-{
-  'user_img': 'img/male.jpg',
-  'lastname': 'Cech',
-  'firstname': 'Noah',
-  'birth': '01.01.1982',
-  'mail': 'noah.cech@gmail.com',
-},
-{
-  'user_img': 'img/male2.jpg',
-  'lastname': 'Marqiuis',
-  'firstname': 'Pascal',
-  'birth': '09.01.1980',
-  'mail': 'pascal.marqius@gmail.com',
-},
-{
-  'user_img': 'img/female3.jpg',
-  'lastname': 'Parker',
-  'firstname': 'Bethany',
-  'birth': '15.12.1989',
-  'mail': 'bethany.parker@gmail.com',
-},
-{
-  'user_img': 'img/male3.jpg',
-  'lastname': 'Ono',
-  'firstname': 'Yuki',
-  'birth': '06.06.1994',
-  'mail': 'yuki.ono@gmail.com',
-},
+let users = [
+  {
+    'user_img': 'img/female.jpg',
+    'lastname': 'Kruse',
+    'firstname': 'Maren',
+    'birth': '20.04.1983',
+    'mail': 'maren.kruse@gmail.com',
+  }, {
+    'user_img': 'img/female2.jpg',
+    'lastname': 'Meier',
+    'firstname': 'Kathrin',
+    'birth': '09.10.1990',
+    'mail': 'kathrin.meier@gmail.com',
+  },
+  {
+    'user_img': 'img/male.jpg',
+    'lastname': 'Cech',
+    'firstname': 'Noah',
+    'birth': '01.01.1982',
+    'mail': 'noah.cech@gmail.com',
+  },
+  {
+    'user_img': 'img/male2.jpg',
+    'lastname': 'Marqiuis',
+    'firstname': 'Pascal',
+    'birth': '09.01.1980',
+    'mail': 'pascal.marqius@gmail.com',
+  },
+  {
+    'user_img': 'img/female3.jpg',
+    'lastname': 'Parker',
+    'firstname': 'Bethany',
+    'birth': '15.12.1989',
+    'mail': 'bethany.parker@gmail.com',
+  },
+  {
+    'user_img': 'img/male3.jpg',
+    'lastname': 'Ono',
+    'firstname': 'Yuki',
+    'birth': '06.06.1994',
+    'mail': 'yuki.ono@gmail.com',
+  },
 ];
 
 setURL('http://thomas-tritscher.developerakademie.com/Mobile-Chat/backend');
@@ -53,14 +54,13 @@ async function loadData() {
   await downloadFromServer();
   console.log('Downloaded data from server');
   messages = JSON.parse(backend.getItem('message')) || [];
-  users = JSON.parse(backend.getItem('user_img')) || [];
+  // users = JSON.parse(backend.getItem('user_img')) || [];
 
 
 }
 
 
-async function loadUser() {
-  await loadData();
+function loadUser() {
   for (let i = 0; i < users.length; i++) {
     let user = users[i];
     document.getElementById('user-dashboard').innerHTML += `
@@ -93,22 +93,8 @@ function addMessage() {
       ;
     clearInputField();
   }
-}
-function addMessageUserTwo() {
-  let message_text = document.getElementById
-    ('message_input').value;
-  messages.push(message_text);
-  saveArraytoLocalStorage('messages', messages);
-  let message = document.getElementById('dialog_section');
-  message.innerHTML = '';
-  for (let i = 0; i < messages.length; i++) {
-    message.innerHTML +=
-      `<div class="message-line-user-two">${messages[i]}</div>`
-      ;
-    clearInputField();
-  }
-}
 
+}
 function clearInputField() {
   document.getElementById('message_input').value = '';
 }
